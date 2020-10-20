5c84338c08440e4989c9e3cc88b1d6482f0a1ac8
Mark-Kohrman <mark.kohrman@gmail.com>
2020-10-19 20:48:07 -0500

    adding rgbSplit function to see if merge works

--- scripts.js
+++ scripts.js
@@ -46,6 +46,16 @@ function redEffect(pixels) {
   // adding function to test if pushing works in Github.  It has no functional significance yet.
 }
 
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i + 0]; // RED
+    pixels.data[i + 200] = pixels.data[i + 1]; // GREEN
+    pixels.data[i - 550] = pixels.data[i + 2];  // Blue
+  }
+  return pixels;
+  // adding function to test if pushing works in Github.  It has no functional significance yet
+}
+
 /* Hook up event listeners */
 video.addEventListener("click", togglePlay);
 toggle.addEventListener("click", togglePlay);
